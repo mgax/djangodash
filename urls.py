d139cd122b5b5fbb764600abdfd60f5a48de3b43
Alexandru Plugaru <alexandru.plugaru@gmail.com>
2010-08-14 15:16:06 +0300

    Added index page, updated requirements with django piston

--- urls.py
+++ urls.py
@@ -1,7 +1,11 @@
 from django.conf.urls.defaults import *
+from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    (r'^admin/', include(admin.site.urls)),
+
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'index.html'}, name="index"),
+    url(r'^admin/', include(admin.site.urls)),
 )
