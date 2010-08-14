0ec1d64ec23860297f9f90ea5238f89618290ca1
Andrei Bocan <zmaxor@gmail.com>
2010-08-14 15:47:04 +0300

    Serve stuff in debug mode

--- urls.py
+++ urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 from django.contrib import admin
+from django.conf import settings
 admin.autodiscover()
 
 urlpatterns = patterns('',
@@ -9,3 +10,9 @@ urlpatterns = patterns('',
         {'template': 'index.html'}, name="index"),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += patterns('',
+    (r'^media/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.MEDIA_ROOT}),
+)
+
