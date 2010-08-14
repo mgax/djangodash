1536a4cdf519b72cdb3bb267ae6bf14e64dedcc9
Alex Morega <alex@grep.ro>
2010-08-14 18:45:29 +0300

    nice vertex icons

--- media/js/polyeditor.js
+++ media/js/polyeditor.js
@@ -3,8 +3,15 @@ function createPolyEditor(map, lat, lon) {
   var width = bounds.getSouthWest().lng() - bounds.getNorthEast().lng();
   var delta = width/8;
 
-  var vertices = new google.maps.MVCArray();
+  var icon_url_normal = MEDIA_URL+"images/vertex-normal.png";
+  var icon_url_hover = MEDIA_URL+"images/vertex-hover.png";
+  var icon_normal = new google.maps.MarkerImage(icon_url_normal);
+  var icon_hover = new google.maps.MarkerImage(icon_url_hover);
+  icon_normal.anchor = new google.maps.Point(8, 8);
+  icon_hover.anchor = new google.maps.Point(8, 8);
 
+
+  var vertices = new google.maps.MVCArray();
   createVertex(lat+delta, lon+delta);
   createVertex(lat+delta, lon-delta);
   createVertex(lat-delta, lon-delta);
@@ -16,8 +23,15 @@ function createPolyEditor(map, lat, lon) {
     if(n == null) n = vertices.getLength();
     var coord = new google.maps.LatLng(lat, lon);
     vertices.insertAt(n, coord);
+
     var marker = new google.maps.Marker({ map: map, position: coord,
-                                          draggable: true });
+                                          draggable: true, icon: icon_normal });
+
+    // change icon on mouseover and mouseout
+    google.maps.event.addListener(marker, 'mouseover', function(evt) {
+      marker.setIcon(icon_hover); });
+    google.maps.event.addListener(marker, 'mouseout', function(evt) {
+      marker.setIcon(icon_normal); });
 
     // keep our index up-to-date when vertices are added/removed
     google.maps.event.addListener(vertices, 'insert_at',
@@ -27,8 +41,8 @@ function createPolyEditor(map, lat, lon) {
 
     // update our point in the polygon path
     google.maps.event.addListener(marker, 'drag', function(evt) {
-        var new_coord = evt.latLng;
-        vertices.setAt(n, new_coord);
+      var new_coord = evt.latLng;
+      vertices.setAt(n, new_coord);
     });
   }
 }
