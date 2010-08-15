97b3ce95e8a58e5a1fb51fd45748f0da64160f30
Alex Morega <alex@grep.ro>
2010-08-15 18:54:50 +0300

    wip - show wifis

--- media/js/browse.js
+++ media/js/browse.js
@@ -2,6 +2,7 @@
 
 internets.InternetsBrowser = function(map) {
   google.maps.event.addListener(map, 'idle', refreshInternets);
+  var internets_list = $('ul#internets-list');
 
   return {
   }
@@ -12,45 +13,64 @@ internets.InternetsBrowser = function(map) {
     var data = { top: ne.lat(), right: ne.lng(),
                  bottom: sw.lat(), left: sw.lng() };
 
-    var internets_list = $('ul#internets-list');
-    $.get('/api/lan', data, function(resp) {
-      $('> li', internets_list).each(function() {
-        $(this).data('polygon').setMap(null);
+    $.get('/api/lan', data, function(lan_list) {
+      $.get('/api/wifi', data, function(wifi_list) {
+        $('> li.lan', internets_list).each(function() {
+          $(this).data('polygon').setMap(null);
+        });
+        $('> li.wifi', internets_list).each(function() {
+          $(this).data('marker').setMap(null);
+        });
+        internets_list.empty();
+        $.each(lan_list, function(i, lan_data) { createLan(lan_data); });
+        $.each(wifi_list, function(i, wifi_data) { createWifi(wifi_data); });
       });
-      internets_list.empty();
-      $.each(resp, function(i, lan_data) { createLan(lan_data); });
     });
+  }
 
-    function createLan(lan_data) {
-      var points = new google.maps.MVCArray();
-      $.each(JSON.parse(lan_data['geo']['points_json']), function(i, p) {
-        points.push(new google.maps.LatLng(p.lat, p.lon));
-      });
-      var polygon = new google.maps.Polygon({ map: map, paths: points });
-      google.maps.event.addListener(polygon, 'click', focusOnLan);
-      var li = $('<li class="lan">').text(lan_data['name']);
-      styleNormal();
-      li.mouseover(styleHighlight);
-      li.mouseout(styleNormal);
-      li.click(focusOnLan);
+  function createWifi(wifi_data) {
+    var icon_url = MEDIA_URL+"images/vertex-normal.png";
+    var icon = new google.maps.MarkerImage(icon_url);
+    icon.anchor = new google.maps.Point(8, 8);
 
-      li.data('polygon', polygon);
-      internets_list.append(li);
+    var coord = new google.maps.LatLng(wifi_data.geo.lat, wifi_data.geo.lon);
+    var marker = new google.maps.Marker({ map: map, position: coord,
+                                          icon: icon });
+    var li = $('<li class="wifi">').text(wifi_data['name']);
+    li.data('marker', marker);
+    internets_list.append(li);
+  }
 
-      function styleHighlight() {
-        polygon.setOptions({fillOpacity: .6});
-      }
+  function createLan(lan_data) {
+    var points = new google.maps.MVCArray();
+    $.each(JSON.parse(lan_data['geo']['points_json']), function(i, p) {
+      points.push(new google.maps.LatLng(p.lat, p.lon));
+    });
+    var polygon = new google.maps.Polygon({ map: map, paths: points });
+    google.maps.event.addListener(polygon, 'click', focusOnLan);
 
-      function styleNormal() {
-        polygon.setOptions({fillOpacity: .3});
-      }
+    var li = $('<li class="lan">').text(lan_data['name']);
+    styleNormal();
+    li.mouseover(styleHighlight);
+    li.mouseout(styleNormal);
+    li.click(focusOnLan);
 
-      function focusOnLan() {
-        var geo = lan_data['geo'];
-        var ne = new google.maps.LatLng(geo['bbox_top'], geo['bbox_right']);
-        var sw = new google.maps.LatLng(geo['bbox_bottom'], geo['bbox_left']);
-        map.fitBounds(new google.maps.LatLngBounds(sw, ne));
-      }
+    li.data('polygon', polygon);
+    internets_list.append(li);
+
+    function styleHighlight() {
+      polygon.setOptions({fillOpacity: .6});
+    }
+
+    function styleNormal() {
+      polygon.setOptions({fillOpacity: .3});
+    }
+
+    function focusOnLan() {
+      var geo = lan_data['geo'];
+      var ne = new google.maps.LatLng(geo['bbox_top'], geo['bbox_right']);
+      var sw = new google.maps.LatLng(geo['bbox_bottom'], geo['bbox_left']);
+      map.fitBounds(new google.maps.LatLngBounds(sw, ne));
     }
   }
 }
